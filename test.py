8064383594faa329c57e8ad3568f13094aa82610
wangjiaming01 <wangjiaming01@baidu.com>
2018-10-10 10:05:20 +0800

    test

---
+++ test.py
@@ -0,0 +1,12 @@
+# -*- coding=utf-8 -*-
+"""
+#
+# File Name: test.py
+# @Author: @baidu.com
+# Copyright (c) 2018 Baidu.com, Inc. All Rights Reserved
+#
+"""
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
